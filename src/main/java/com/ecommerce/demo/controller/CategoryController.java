86b783c17e453914067b831935549db6d9b290c9
Night <dranhata@gmail.com>
2023-09-15 20:12:21 +0700

    #3 Edit Category

--- src/main/java/com/ecommerce/demo/controller/CategoryController.java
+++ src/main/java/com/ecommerce/demo/controller/CategoryController.java
@@ -1,8 +1,11 @@
 package com.ecommerce.demo.controller;
 
+import com.ecommerce.demo.common.ApiResponse;
 import com.ecommerce.demo.model.Category;
 import com.ecommerce.demo.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,13 +18,23 @@ public class CategoryController {
     CategoryService categoryService;
 
     @PostMapping("/create")
-    public String createCategory(@RequestBody Category category) {
+    public ResponseEntity<ApiResponse> createCategory(@RequestBody Category category) {
         categoryService.createCategory(category);
-        return "success";
+        return new ResponseEntity<>(new ApiResponse(true, "a new category created"), HttpStatus.CREATED);
     }
 
     @GetMapping("/list")
     public List<Category> listCategory() {
         return categoryService.listCategory();
+    }
+
+    @PostMapping("/update/{categoryId}")
+    public ResponseEntity<ApiResponse> updateCategory(@PathVariable("categoryId") int categoryId, @RequestBody Category category ) {
+        System.out.println("category id " + categoryId);
+        if (!categoryService.findById(categoryId)) {
+            return new ResponseEntity<ApiResponse>(new ApiResponse(false, "category does not exists"), HttpStatus.NOT_FOUND);
+        }
+        categoryService.editCategory(categoryId, category);
+        return new ResponseEntity<ApiResponse>(new ApiResponse(true, "category has been updated"), HttpStatus.OK);
     }
 }
