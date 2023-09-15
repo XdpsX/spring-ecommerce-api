898d8f2f680f1aeb3c4cf69490e89112076009ca
Night <dranhata@gmail.com>
2023-09-15 21:51:57 +0700

    #8 wish list

--- src/main/java/com/ecommerce/demo/model/Product.java
+++ src/main/java/com/ecommerce/demo/model/Product.java
@@ -1,5 +1,7 @@
 package com.ecommerce.demo.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 
@@ -19,6 +21,7 @@ public class Product {
 
     // Many to one relationship
     @ManyToOne
+    @JsonIgnore
     @JoinColumn(name = "category_id")
     Category category;
 
